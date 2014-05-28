c86a752cdf0d594eecebbc53eb85854f9736f0fa
songjianbang <jianbangsong@jianbangdeMacBook-Air.local>
2014-05-28 10:24:23 +0800

    222

--- Tetris/Tetris/render.cpp
+++ Tetris/Tetris/render.cpp
@@ -9,3 +9,28 @@
 #include "render.h"
 
 const PictureHandle INVALIDATE_PICTURE = 0;
+
+void CRender::DrawPicture(PictureHandle hPic, CRect & dest)
+{
+    if (m_batches.empty() || m_batches.back().hPic != hPic) {
+        Batch b;
+        b.hPic = hPic;
+        b.vertices.push_back(Vertex());
+        b.vertices.push_back(Vertex());
+        b.vertices.push_back(Vertex());
+        b.vertices.push_back(Vertex());
+        
+        m_batches.push_back(b);
+        return;
+    }
+    
+    if (m_batches.back().hPic == hPic) {
+        Batch& b = m_batches.back();
+        b.vertices.push_back(Vertex());
+        b.vertices.push_back(Vertex());
+        b.vertices.push_back(Vertex());
+        b.vertices.push_back(Vertex());
+        
+        return;
+    }
+}
--- Tetris/Tetris/render.h
+++ Tetris/Tetris/render.h
@@ -11,6 +11,8 @@
 
 #include <iostream>
 
+#include <vector>
+
 typedef int PictureHandle;
 extern const PictureHandle INVALIDATE_PICTURE;
 
@@ -38,6 +40,19 @@ struct CColor
     float r,g,b,a;
 };
 
+struct Vertex
+{
+    float x,y,z;
+    float r,g,b,a;
+    float tx,ty;
+};
+
+struct Batch
+{
+    PictureHandle hPic;
+    std::vector<Vertex> vertices;
+};
+
 class CRender
 {
 public:
@@ -54,6 +69,10 @@ public:
     
     void DrawPicture(PictureHandle hPic, CRect & dest);
     void DrawPicture(PictureHandle hPic, DrawPictureParameter& dpp);
+    
+private:
+    std::vector<Vertex> m_vertexes;
+    std::vector<Batch>  m_batches;
 };
 
 #endif /* defined(__Tetris__render__) */
